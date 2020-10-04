7f0fc35695f1bf9a0cca13129b044349a8b8ec2d
C300AMG <5.realy@gmail.com>
2020-10-04 18:20:26 +0700

    ok

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,9 @@
       <template>
     <div>
       <a-back-top />
-      Scroll down to see the bottom-right
+      <!-- Scroll down to see the bottom-right
       <strong style="color: rgba(64, 64, 64, 0.6)"> gray </strong>
-      button.
+      button. -->
     </div>
   </template>
   </div>
